6fe37d7af22111fea8ff8fc30f397aa8b62e2b89
Lepsps <122830654+Lepsps@users.noreply.github.com>
2024-11-06 18:05:17 +0300

    Add files via upload

---
+++ include/add.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#ifndef add_h
+	#define add_h
+
+int add(int x, int y);
+
+
+#endif
---
+++ tests/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "add.h"
+
+int main() {
+	int res = add(6, 9);
+	std::cout << res << " is result" << std::endl;
+	
+	return 0;
+}
